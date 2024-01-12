d2c40101a720086dc156fa74e53418a865354f27
ChunkyMonkey00 <148571454+ChunkyMonkey00@users.noreply.github.com>
2024-01-12 13:16:41 -0500

    Update demo.js

--- demo.js
+++ demo.js
@@ -42,3 +42,32 @@ normVar.value = 6; // Logs: Value is equal to 6
 normVar.value = 7; // Logs: Value is equal to 7
 normVar.value = 8; // Logs: Value is equal to 8
 obj.value = 11; // Logs: Value is greater than 10, This also runs
+
+//Update v0.0.2 (remove and multiple conditions) demos
+let var1 = cwv(0);
+let var2 = cwv(0);
+
+let watcher5 = new Watcher({
+  condition: val => val > 69,
+  callback: () => console.log('Value is greater than 69'),
+  variable: var1
+});
+
+var1.value = 70; // Logs: Value is greater than 5
+
+watcher5.remove();
+
+var1.value = 71; // Nothing happens
+
+
+let watcher6 = new Watcher({
+  condition: val => [val > 10, val < -10, val == 0.32],
+  callback: () => {
+    console.log("One condition was met.");
+  },
+  variable: var2
+});
+
+var2.value = 11; // Triggers condition
+var2.value = -11; // Triggers condition
+var2.value = 0.32; // Triggers condition
